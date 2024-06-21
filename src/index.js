4b20a7ab382e6519b1f96834f51cf08a65172cc5
sysssyss03 <sy-ss@outlook.com>
2024-06-21 16:57:58 +0800

    update js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 import DOCS from './help.html'
- 
-// return docs
-if (url.pathname === "/") {
-  return new Response(DOCS, {
-    status: 200,
-    headers: {
-      "content-type": "text/html"
-    }
-  });
-}
-
 
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
@@ -54,6 +43,15 @@ async function handleRequest(request) {
         status: 404,
       }
     );
+  }
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
   }
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
