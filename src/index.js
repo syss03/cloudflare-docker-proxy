b38c901c9595761a8a1ea1d5323c5ab0b04fd5fa
sysssyss03 <sy-ss@outlook.com>
2024-06-21 16:47:30 +0800

    update website url

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import DOCS from './help.html'
+ 
+// return docs
+if (url.pathname === "/") {
+  return new Response(DOCS, {
+    status: 200,
+    headers: {
+      "content-type": "text/html"
+    }
+  });
+}
+
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -7,16 +20,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.mroj.top": dockerHub,
+  "quay.mroj.top": "https://quay.io",
+  "gcr.mroj.top": "https://gcr.io",
+  "k8s-gcr.mroj.top": "https://k8s.gcr.io",
+  "k8s.mroj.top": "https://registry.k8s.io",
+  "ghcr.mroj.top": "https://ghcr.io",
+  "cloudsmith.mroj.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.mroj.top": dockerHub,
 };
 
 function routeByHosts(host) {
